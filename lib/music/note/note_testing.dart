f173f045d41e276b6ce0cf16754f13585f67fbba
Brad Odenath <brad.odenath@gmail.com>
2023-01-27 03:19:31 -0500

    Testing file

--- lib/music/note/note_testing.dart
+++ lib/music/note/note_testing.dart
@@ -121,7 +121,7 @@ void test() {
           "C#5").toString());
 
   /// TODO: FIX THIS
-  /// print("TODO: FIX THIS");
+  print("TODO: FIX THIS");
   print('Note.notesToFingerboardCoordinates('
       'Chordophone.fromDefaultChordophoneStringTuning(), '
       '["E4","D#4","B3","G#3","B2","E2"]) => \n\t'
