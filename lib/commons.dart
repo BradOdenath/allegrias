8ed40cea633ad88557ba62ed4e17074fdddacc85
Brad Odenath <brad.odenath@gmail.com>
2023-01-28 02:55:53 -0500

    hmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmm

--- lib/commons.dart
+++ lib/commons.dart
@@ -1,7 +1,7 @@
 // ignore_for_file: non_constant_identifier_names
 
-bool debug = true;
-bool test = false;
+bool debug = false;
+bool test = true;
 
 void print_debug(statement) =>
     ((debug) ? print(statement) : null);
